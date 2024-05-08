905e1dcbc2b8c2e5fba73106655570f2428d50dc
Bregy Malpartida <bregy@minsky.cc>
2024-05-08 00:46:44 -0500

    feat: Refactor Arduino code for improved readability and maintainability

--- er_receiver/ER_RECEIVER.ino
+++ er_receiver/ER_RECEIVER.ino
@@ -1,18 +1,17 @@
 #include "arduino_secrets.h"
 #include "thingProperties.h"
 
-const int ledPins[] = {2,3,4,5,6,7,14,15,16,17,18,19};
+const int ledPins[] = {13, 12, 11, 10, 9,  8,  7,  6,  5,
+                       4,  3,  2,  14, 15, 16, 17, 21, 20};
+
 const int numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
 
 void setup() {
-  // Initialize serial and wait for port to open:
   Serial.begin(9600);
-  delay(1500); 
+  delay(1500);
 
-  // Defined in thingProperties.h
   initProperties();
 
-  // Connect to Arduino IoT Cloud
   ArduinoCloud.begin(ArduinoIoTPreferredConnection);
   setDebugMessageLevel(2);
   ArduinoCloud.printDebugInfo();
@@ -21,10 +20,9 @@ void setup() {
 void loop() {
   ArduinoCloud.update();
 
-  int veleta_lima2 = map(veleta, 0, 360, 0, 5); // random(5);
-  int anemometro_lima2 = map(anemometro, 0, 7, 0, 90); // random(90);
+  int veleta_lima2 = map(veleta, 0, 360, 0, 5);
+  int anemometro_lima2 = map(anemometro, 0, 7, 0, 90);
 
-  
   Serial.print("#");
   Serial.print(ledPins[veleta_lima2]);
   Serial.print("$");
@@ -37,18 +35,5 @@ void loop() {
   delay(1000);
 }
 
-
-/*
-  Since VeletaLima is READ_WRITE variable, onVeletaLimaChange() is
-  executed every time a new value is received from IoT Cloud.
-*/
-void onVeletaLimaChange()  {
-  // Add your code here to act upon VeletaLima change
-}
-/*
-  Since AnemometroLima is READ_WRITE variable, onAnemometroLimaChange() is
-  executed every time a new value is received from IoT Cloud.
-*/
-void onAnemometroLimaChange()  {
-  // Add your code here to act upon AnemometroLima change
-}
+void onVeletaLimaChange() {}
+void onAnemometroLimaChange() {}
